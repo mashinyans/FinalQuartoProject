15b7d7f80a034fc794bafbe0428de7b204a60ee2
mashinyans <srbuhi_mashinyan@edu.aua.am>
2024-04-03 14:02:20 +0400

    Class Design 1

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,18 @@
 public class Board {
-    // A 4x4 grid representing the board, initialized to null indicating empty positions.
-    // Method for placing a piece on the board at a defined position.
-    // Method for determining if a certain position on the board is occupied.
-    // Method to check for a winning condition, using logic to evaluate whether there is a line of four pieces with at least one attribute in common.
+
+        public static final int SIZE = 4;
+        public static final char EMPTY = '.';
+
+        private char[][] quartoBoard;
+        public Board() {
+            quartoBoard = new char[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    quartoBoard[i][j] = EMPTY;
+                }
+            }
+        }
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 public class Main{
-    //controls interactions between players and the game board.
-    //Srbuhi
-    //Nune
-    //AniA
-    //Hovag
+
+
 }
