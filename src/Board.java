9c0f116e09f439f6c41eb783e2885d8877123874
mashinyans <srbuhi_mashinyan@edu.aua.am>
2024-04-03 14:37:51 +0400

    Class Design 1

--- src/Board.java
+++ src/Board.java
@@ -1,36 +1,47 @@
 public class Board {
 
-        public static final int SIZE = 4;
-        public static final char EMPTY = '.';
-//boardd
-        private char[][] quartoBoard;
-        public Board() {
-            quartoBoard = new char[SIZE][SIZE];
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    quartoBoard[i][j] = EMPTY;
-                }
+    public static final int SIZE = 4;
+    public static final char EMPTY = '.';
+    //boardd
+    private char[][] quartoBoard;
+
+    public Board() {
+        quartoBoard = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                quartoBoard[i][j] = EMPTY;
             }
         }
-        public Board(char[][] quartoBoard){
-            this.quartoBoard=new char[SIZE][SIZE];
-            for(int i=0;i<SIZE;i++){
-                for(int j=0;j<SIZE;j++){
-                    this.quartoBoard[i][j]=quartoBoard[i][j];
-                }
+    }
+
+    public Board(char[][] quartoBoard) {
+        this.quartoBoard = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                this.quartoBoard[i][j] = quartoBoard[i][j];
+            }
+        }
+    }
+
+    private static char[][] copyArray(char[][] board) {
+        char[][] board1 = new char[board.length][board[0].length];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board1[i][j] = board[i][j];
             }
         }
 
-        public Board(Board that) {
-            this(that.quartoBoard);
-            this.quartoBoard = new char[that.quartoBoard.length][that.quartoBoard[0].length];
-            for(int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    this.quartoBoard[i][j] = that.quartoBoard[i][j];
-                }
-            }
-        }
+        return board1;
+    }
+    public char getPosition(int i, int j) {
+        return quartoBoard[i][j];
+    }
+
+    public char[][] getBoard() {
+        return copyArray(quartoBoard);
+    }
+
+
+
 
 }
-
-
