9a6c1bba60658080d1677703cf6094f13824fabf
nunetrdatyan <nune_trdatyan@edu.aua.am>
2024-04-13 15:45:35 +0400

    board

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,25 @@
-public class Player {
-    // Attributes for player details (names, scores).
+public abstract class Player {
+
+    public String name;
+    public boolean Winner;
+
+    public Player(String name) {
+        this.name = name;
+        this.Winner = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isWinner() {
+        return Winner;
+    }
+
+    public void setWinner(boolean winner) {
+        this.Winner = winner;
+    }
+
+    // Abstract method to make a move, forcing subclasses to implement it
+    public abstract int[] makeMove();
 }
