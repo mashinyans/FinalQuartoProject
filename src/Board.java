da6f4bdece417dca21d85f7502b380cedd79a0bb
mashinyans <srbuhi_mashinyan@edu.aua.am>
2024-04-13 15:41:49 +0400

    Class Design 1

--- src/Board.java
+++ src/Board.java
@@ -1,68 +1,102 @@
 public class Board {
 
+    private static final String[] BOARD_ROW = {"A", "B", "C", "D"};
+    private static final String[] BOARD_COL = {"1", "2", "3", "4"};
     public static final int SIZE = 4;
     public static final char EMPTY = '.';
-    //board
-    private char[][] quartoBoard;
+    private int row;
+    private int col;
+
+    private char[][] grid;
 
     public Board() {
-        quartoBoard = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                quartoBoard[i][j] = EMPTY;
+        grid = new char[SIZE][SIZE];
+        initializeGrid();
+    }
+
+    private void initializeGrid() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                grid[row][col] = EMPTY;
             }
         }
     }
 
-    public Board(char[][] quartoBoard) {
-        this.quartoBoard = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                this.quartoBoard[i][j] = quartoBoard[i][j];
+    public void printBoard() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(grid[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+   /* public Board(Board other) {
+        this.setRow(other.row);
+        this.setCol(other.col);
+    }*/
+
+    private Board(int rank, int file) {
+        this.setRow(rank);
+        this.setCol(file);
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public void setRow(int row) {
+        if (row >= 0 && row < SIZE)
+            this.row = row;
+    }
+
+    public void setCol(int col) {
+        if (col >= 0 && col < SIZE)
+            this.col = col;
+    }
+
+    public String toString() {
+        return " " + (SIZE + this.col) + (SIZE - this.row);
+    }
+
+    public static Board checkColRow(int col, int row) {
+        if ((col < 0 || col > 7) || (row < 0 || row > 7)) {
+            return null;
+        }
+
+        return new Board(col, row);
+    }
+
+    public static Board checkColRowString(String position) {
+        if (position.length() != 2) {
+            return null;
+        }
+        int row = 0;
+        int col = 0;
+
+        String colString = position.substring(0, 1);
+        String rowString = position.substring(1, 2);
+
+        for (int i = 0; i <= 7; i++) {
+            if (colString.equals(BOARD_COL[i])) {
+                col = i;
+            }
+            if (rowString.equals(BOARD_ROW[i])) {
+                row = i;
             }
         }
+
+        return checkColRow(col, row);
     }
 
-    private static char[][] copyArray(char[][] board) {
-        char[][] board1 = new char[board.length][board[0].length];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                board1[i][j] = board[i][j];
-            }
-        }
-
-        return board1;
-    }
-    public char getPosition(int i, int j) {
-        return quartoBoard[i][j];
-    }
-
-    public char[][] getBoard() {
-        return copyArray(quartoBoard);
-    }
-
-    public void printTheBoard() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++);
-        }
-        System.out.println();
-    }
-    public boolean submitMove(char player,int i,int j){
-        if(i>=0&&i<SIZE&&j>=0&&j<SIZE&&quartoBoard[i][j]==EMPTY){
-            quartoBoard[i][j]=player;
-            return true;
-        }
-        return false;
-    }
-    public boolean isBoardFull() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (quartoBoard[i][j] == EMPTY) {
-                    return false;
-                }
-            }
-        }
-        return true;
+    public boolean isValidPosition(int row, int col) {
+        return row >= 0 && row < SIZE && col >= 0 && col < SIZE;
     }
 
 }
+
